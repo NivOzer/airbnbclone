a706535baf8de43b3bb5240d7e4e3d6f15c3d77d
Niv Ozer <niv.ozer@yahoo.com>
2023-12-29 16:07:25 +0200

    Card - Navbar compnents

---
+++ src/components/Card.js
@@ -0,0 +1,9 @@
+import React from "react";
+import cardprofilepic from '../images/cardimage.png'
+export default function Card(){
+    return(
+        <div>
+            <img src ={cardprofilepic} className="card-img"></img>
+        </div>
+    )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+import airbnblogo from "../images/airbnblogo.png"
+export default function Navbar(){
+    return(
+        <nav className="navbar"> 
+        <img src = {airbnblogo}></img>
+        </nav>
+    )
+}
